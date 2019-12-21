c15f4d0c9ca1cb8af33608820dc3bfcefc252a89
King <302568896@qq.com>
2019-12-21 22:48:49 +0800

    切换分支

--- lab1/boot.h
+++ lab1/boot.h
@@ -1,2 +1,10 @@
 #ifndef _BOOT_H
 #define _BOOT_H
+
+#define SEG_NULL                            \
+    .word 0, 0;                             \
+    .byte 0, 0, 0, 0
+
+#define SEG_ASM(type,base,lim)              \
+    .word (((lim) >> 12)& 0xffff), ((base) & 0xffff);   \
+    .byte
