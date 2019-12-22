ba257694f6cf53bb58ee09b46c604e7c7154ae78
root <root@L.localdomain>
2019-12-22 10:30:51 +0800

    第一次整合，尝试进入保护模式，调用C

--- lab1/boot.h
+++ lab1/boot.h
@@ -7,4 +7,14 @@
 
 #define SEG_ASM(type,base,lim)              \
     .word (((lim) >> 12)& 0xffff), ((base) & 0xffff);   \
-    .byte
+    .byte (((base) >> 16) & 0xff), (0x90 | (type)),     \
+        (0xC0 | (((lim) >> 28) & 0xf)), (((base) >> 24) & 0xff)
+
+#define STA_X       0x08     // Executable segment
+#define STA_E       0x04     // Expand down (non-executable segments)
+#define STA_C       0x04     // Conforming code segment (executable only)
+#define STA_W       0x02     // Writeable (non-executable segments)
+#define STA_R       0x02     // Readable (executable segments)
+#define STA_A       0x01     // Accessed
+
+#endif
--- lab1/boot.s
+++ lab1/boot.s
@@ -1,3 +1,11 @@
+#include <boot.h>
+
+vga_addr = 0xb800
+vga_nber = 0x4000
+PROT_NODE_CSEG = 0x8
+PROT_NODE_DSEG = 0x10
+4GB		 = 0xffffffff
+
 .global start
 start:
 	.code16
@@ -9,7 +17,17 @@ start:
 	movw	%ax,%ds
 	movw	%ax,%es
 	movw	%ax,%ss
+	xorw	%sp,%sp
+	call 	vga_clr
 
+vga_clr:				#clear vga
+	movw 	$vga_addr,%bx
+	movw 	%bx,%es
+	xorw 	%di,%di
+	movw 	$0x0000,%ax
+	movw	$vga_nber,%cx
+	rep 	stosb
+	ret
 #打开A20
 seta20.1:
 	inb		$0x64,%al	#等待端口不忙
@@ -29,8 +47,50 @@ seta20.2:
 	orl 	$0x1, %eax 	#打开保护模式
 	movl	%eax, %cr0
 
+	ljmp 	$PROT_NODE_CSEG, $protcseg
 
+.code32	
+	movw $PROT_MODE_DSEG, %ax                       # Our data segment selector
+    movw %ax, %ds                                   # -> DS: Data Segment
+    movw %ax, %es                                   # -> ES: Extra Segment
+    movw %ax, %fs                                   # -> FS
+    movw %ax, %gs                                   # -> GS
+    movw %ax, %ss                                   # -> SS: Stack Segment
+
+    # Set up the stack pointer and call into C. The stack region is from 0--start(0x7c00)
+    movl $0x0, %ebp
+    movl $start, %esp
+
+boot_info:				#printf info of boot
+	movw 	$msg,%si
+	movb 	$0x7,%ah
+	movw	$vga_addr,%bx
+	movw	%bx,%es
+	movl 	$985,%edx	
+vga_prf:				#printf
+	lodsb
+	or 		%al,%al
+	jz 		c_boot
+	movw	%ax,%es:0(,%edx,2)
+	incl	%edx
+	jmp		vga_prf
+	
+c_boot:
+	call bootmain
+spin:
+	jmp spin
+msg:
+.asciz	"Welcome to XOS(protect mode)!"
 
 .p2align 2
 gdt:
-	
+	SEG_NULL
+	SEG_ASM(STA_X|STA_R, 0x0, 4GB)
+	SRG_ASM(STA_W, 0x0, 4GB)
+
+gdtdsec:
+	.word 0x0017
+	.long gdt
+
+.org 510
+.word 0x55aa
