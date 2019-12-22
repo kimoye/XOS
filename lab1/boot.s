978820fbd18d13bfe4a861412e6f48ca6cc95c90
King <302568896@qq.com>
2019-12-22 15:50:12 +0800

    Update boot.s

--- lab1/boot.s
+++ lab1/boot.s
@@ -1,16 +1,15 @@
-#include <boot.h>
+#include "boot.h"
 
-vga_addr = 0xb800
+vga_addr = 0xb8000
 vga_nber = 0x4000
-PROT_NODE_CSEG = 0x8
-PROT_NODE_DSEG = 0x10
-4GB		 = 0xffffffff
+PROT_MODE_CSEG = 0x8
+PROT_MODE_DSEG = 0x10
 
 .global start
 start:
 	.code16
-	cli					#关中断，网上学的
-	cld					#清楚方向标志，网上学的
+	cli						#关中断，网上学的
+	cld						#清楚方向标志，网上学的
 	
 	#清零段寄存器
 	xorw	%ax,%ax
@@ -18,22 +17,13 @@ start:
 	movw	%ax,%es
 	movw	%ax,%ss
 	xorw	%sp,%sp
-	call 	vga_clr
 
-vga_clr:				#clear vga
-	movw 	$vga_addr,%bx
-	movw 	%bx,%es
-	xorw 	%di,%di
-	movw 	$0x0000,%ax
-	movw	$vga_nber,%cx
-	rep 	stosb
-	ret
 #打开A20
 seta20.1:
-	inb		$0x64,%al	#等待端口不忙
-	testb	$0x2, %al 	#如果al第2位为0就不执行跳转
+	inb		$0x64,%al		#等待端口不忙
+	testb	$0x2, %al 		#如果al第2位为0就不执行跳转
 	jnz		seta20.1
-	movb	$0xd1,%al	#0xd1 --> port 0x64
+	movb	$0xd1,%al		#0xd1 --> port 0x64
 	outb 	%al, $0x64
 seta20.2:
 	inb 	$0x64,%al
@@ -41,56 +31,76 @@ seta20.2:
 	jnz		seta20.2
 	movb	$0xdf,%al
 	outb	%al,$0x60
-	#load gdt
-	lgdt	gdtdsec
+	
+	lgdt	gdtdsec			#load gdt
+
 	movl	%cr0, %eax
-	orl 	$0x1, %eax 	#打开保护模式
+	orl 	$0x1, %eax 		#打开保护模式
 	movl	%eax, %cr0
 
-	ljmp 	$PROT_NODE_CSEG, $protcseg
+	ljmp 	$PROT_MODE_CSEG, $protcseg
 
-.code32	
+.code32
+protcseg:
 	movw $PROT_MODE_DSEG, %ax                       # Our data segment selector
     movw %ax, %ds                                   # -> DS: Data Segment
     movw %ax, %es                                   # -> ES: Extra Segment
-    movw %ax, %fs                                   # -> FS
-    movw %ax, %gs                                   # -> GS
+    movw %ax, %fs                                   # -> FS:Flag Segment
+    movw %ax, %gs                                   # -> GS:
     movw %ax, %ss                                   # -> SS: Stack Segment
 
     # Set up the stack pointer and call into C. The stack region is from 0--start(0x7c00)
     movl $0x0, %ebp
     movl $start, %esp
 
-boot_info:				#printf info of boot
-	movw 	$msg,%si
-	movb 	$0x7,%ah
-	movw	$vga_addr,%bx
-	movw	%bx,%es
-	movl 	$985,%edx	
-vga_prf:				#printf
-	lodsb
-	or 		%al,%al
-	jz 		c_boot
-	movw	%ax,%es:0(,%edx,2)
-	incl	%edx
-	jmp		vga_prf
-	
+disp:
+  movl $msg, %edi
+  call puts
+  jmp  c_boot
+
+# Print a line of string to screen.
+# Parameter:
+# %edi: address of NUL-terminated string that is to be printed.
+puts:
+  call clear
+  xorl %ecx, %ecx
+puts.0:
+  movb (%edi,%ecx), %dl
+  test %dl, %dl
+  jz   puts.1
+  movb $0xf, %dh
+  movw %dx, (%ebx,%ecx,2)
+  incl %ecx
+  jmp  puts.0
+puts.1:
+  ret
+
+# Clear screen.
+clear:
+  xorl %ecx, %ecx
+  movl $0xb8000, %ebx
+clear.0:
+  movl $0x0f200f20, (%ebx,%ecx,4)
+  incl %ecx
+  cmpl $1000, %ecx
+  jl   clear.0
+  ret
 c_boot:
-	call bootmain
-spin:
-	jmp spin
+	jmp .
+#------------------指令无关域---------------------------#
 msg:
 .asciz	"Welcome to XOS(protect mode)!"
 
 .p2align 2
 gdt:
 	SEG_NULL
-	SEG_ASM(STA_X|STA_R, 0x0, 4GB)
-	SRG_ASM(STA_W, 0x0, 4GB)
+	SEG_ASM(STA_X|STA_R, 0x0, 0xffffffff)
+	SEG_ASM(STA_W, 0x0, 0xffffffff)
 
 gdtdsec:
-	.word 0x0017
+	.word .-gdt-1
 	.long gdt
 
 .org 510
-.word 0x55aa
+.byte 0x55
+.byte 0xaa
